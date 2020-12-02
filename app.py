d8f57f919e5cabf287c0cd806ec1489ac06f3fd6
aprilha3097 <aprilha3097@gmail.com>
2020-12-02 17:19:34 +0000

    Add for loop to iterate through recipes for home page

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_home():
+def index():
     recipes = list(mongo.db.recipes.find())
-    return render_template("recipes.html", recipes=recipes)
+    return render_template("index.html", recipes=recipes)
 
 
 if __name__ == "__main__":
